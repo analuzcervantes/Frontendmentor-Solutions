f7f10aa3d150fbbb800bc48a37b3810ee1d27431
Ana Luz Cervantes <analuzcervantes.s@gmail.com>
2021-03-11 13:00:51 -0700

    Add Testimonials slider

---
+++ coding-bootcamp-testimonials-slider-master/js/main.js
@@ -0,0 +1,32 @@
+const next = document.querySelector('.next');
+const prev = document.querySelector('.previous');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+display(index);
+
+function display(index) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex';
+}
+
+function nextSlide() {
+    index++;
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+    display(index);
+}
+
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    display(index);
+}
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
